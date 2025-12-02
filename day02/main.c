f7a09b5ddb204f4f81e958cbca1b84737dbb7fc9
Adam Strojek <adam@strojek.info>
2025-12-02 22:30:05 +0100

    Solution for Day 2 Part 2

--- day02/main.c
+++ day02/main.c
@@ -18,26 +18,42 @@ int main(int argc, char *argv[]) {
   }
 
   long res1 = 0;
+  long res2 = 0;
 
   long range_start, range_end;
   char drop;
 
+  size_t primes[] = {2, 3, 5, 7};
+
   while (fscanf(file, "%ld-%ld%c", &range_start, &range_end, &drop) > 0) {
-    int range_len = range_end - range_start;
-    printf("Range: %ld - %ld, len: %d\n", range_start, range_end, range_len);
     for (long i = range_start; i <= range_end; ++i) {
       char num[255];
       sprintf(num, "%ld", i);
       size_t len = strlen(num);
-      size_t len_half = len / 2;
-      if (len % 2 == 0 && strncmp(num, num + len_half, len_half) == 0) {
-        printf("%s len: %lu\n", num, strlen(num));
-        res1 += i;
+      for (int p = 0; p < 4; ++p) {
+        if (len % primes[p] == 0) {
+          size_t part_len = len / primes[p];
+          bool valid = true;
+          for (size_t x = 0; x < primes[p]; ++x) {
+            if (strncmp(num, num + part_len * x, len / primes[p]) != 0) {
+              valid = false;
+              break;
+            }
+          }
+          if (valid) {
+            if (p == 0) {
+              res1 += i;
+            }
+            res2 += i;
+            break;
+          }
+        }
       }
     }
   }
 
   printf("Result for part 1: %ld\n", res1);
+  printf("Result for part 2: %ld\n", res2);
 
   fclose(file);
   return 0;
