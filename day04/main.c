312c1fb4a31db087d4f6b80ae8d0967c7cd32045
Adam Strojek <adam@strojek.info>
2025-12-04 13:05:04 +0100

    Solution Day 4 Part 2

--- day04/main.c
+++ day04/main.c
@@ -4,21 +4,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-int locations[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
-                       {0, 1},   {1, -1}, {1, 0},  {1, 1}};
+int8_t locations[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
+                          {0, 1},   {1, -1}, {1, 0},  {1, 1}};
 
 #define X 0
 #define Y 1
 
 // Returns numer of adjacted paper rolls
-// Skips if current field is not @ (returns 9)
-int check_adjacent(char *grid, size_t row_size, size_t x, size_t y) {
-  if (x > row_size || y > row_size) {
-    return 0;
-  }
-
-  if (grid[y * row_size + x] != '@') {
-    // printf("%dx%d %c\n", y, x, grid[y * row_size + x]);
+// Returns 9 if current field is not @ or is out of range
+int8_t check_adjacent(char *grid, size_t row_size, size_t x, size_t y) {
+  if (x > row_size || y > row_size || grid[y * row_size + x] != '@') {
     return 9;
   }
 
@@ -31,14 +26,8 @@ int check_adjacent(char *grid, size_t row_size, size_t x, size_t y) {
     if (pos_x < 0 || pos_y < 0 || (size_t)pos_x >= row_size ||
         (size_t)pos_y >= row_size) {
       continue;
-      // if (y == 4 && x == 9)
-      //   printf("Out of range %dx%d\n", pos_x, pos_y);
-      // Out of range continue;
     }
 
-    // if (y == 4 && x == 9)
-    //   printf("For %02ldx%02ld, checking %2dx%2d: %c\n", x, y, pos_x, pos_y,
-    //          grid[pos_y * row_size + pos_x]);
     if (grid[pos_y * row_size + pos_x] == '@')
       rolls++;
   }
@@ -66,7 +55,7 @@ int main(int argc, char *argv[]) {
   size_t len;
   size_t row = 0;
 
-  char *grid = NULL;
+  char *grid = NULL, *grid2 = NULL;
   size_t size = 0;
 
   while ((line = fgetln(file, &len)) != NULL) {
@@ -74,6 +63,7 @@ int main(int argc, char *argv[]) {
 
     if (grid == NULL) {
       grid = malloc(len * len);
+      grid2 = malloc(len * len);
       size = len;
     }
 
@@ -82,27 +72,33 @@ int main(int argc, char *argv[]) {
   }
 
   printf("%ldx%ld\n", size, row);
+  memcpy(grid2, grid, size * size);
 
-  for (size_t y = 0; y < row; ++y) {
-    for (size_t x = 0; x < size; ++x) {
+  int step_count;
+  do {
+    step_count = 0;
+    for (size_t y = 0; y < row; ++y) {
+      for (size_t x = 0; x < size; ++x) {
 
-      int ret = check_adjacent(grid, size, x, y);
+        int ret = check_adjacent(grid, size, x, y);
 
-      // if (ret == 9) {
-      //   printf(" ");
-      // } else
-      //   printf("%d", ret);
-
-      if (ret < 4)
-        res1++;
+        if (ret < 4) {
+          grid2[y * size + x] = '.';
+          step_count++;
+        }
+      }
     }
-    // printf("x\n");
-  }
+    if (res1 == 0)
+      res1 = step_count;
+    res2 += step_count;
+    memcpy(grid, grid2, size * size);
+  } while (step_count > 0);
 
   printf("Result for part 1: %lld\n", res1);
   printf("Result for part 2: %lld\n", res2);
 
   free(grid);
+  free(grid2);
   fclose(file);
   return 0;
 }
